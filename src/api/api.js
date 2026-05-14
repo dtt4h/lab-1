e5c97b9cbfa65ad727d7f6537cf3e2844176bd54
dtt4h <dtt4h322@gmail.com>
2026-05-14 07:44:55 +0700

    upd

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// URL бекенда можно передать через переменную окружения или прописать явно
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+  baseURL: "http://141.105.66.44:3001",
 });
